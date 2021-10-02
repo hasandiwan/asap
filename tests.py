af7efb8071f3e4640fd0df9e9659b3e3533ea2fc
Hasan Diwan <hd1@jsc.d8u.us>
2021-10-02 07:39:08 -0700

    move testing over to the same heroku sample

--- tests.py
+++ tests.py
@@ -4,6 +4,10 @@ import pytest
 import simplejson as json
 import tempfile
 import timeout_decorator
+import urllib.parse as urlparse
+
+
+HEROKU_URL = os.environ.get("DATABASE_URL")
 
 
 @pytest.fixture
@@ -13,7 +17,18 @@ def client():
         with app.app_context():
             init_db()
         yield client
-    with psycopg2.connect("dbname=asap user=postgres") as con:
+
+    if HEROKU_URL:
+        url = urlparse.urlparse(HEROKU_URL)
+        dbname=url.path[1:]
+        user=url.username
+        password=url.password
+        host=url.hostname
+        port=url.port
+    else:
+        dbname = 'asap'
+        user = 'postgres'
+    with psycopg2.connect(dbname=dbname, user=user, password=password, host=host, port=port) as con:
         with con.cursor() as cursor:
             cursor.execute(
                 "SELECT pid, pg_terminate_backend(pid) FROM pg_stat_activity WHERE datname = current_database() AND pid <> pg_backend_pid();drop table if exists tbl"
